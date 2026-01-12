928a544382d2cd65597226efbc121f24d83f198e
Khanbite <167370853+Khanbite@users.noreply.github.com>
2026-01-12 01:36:06 +0000

    Fixed the basics

--- basics.py
+++ basics.py
@@ -3,6 +3,9 @@ def calculate_discount(price, is_member):
     Returns the discounted price.
     Members receive a 10% discount.
     """
+    if price < 0:
+        raise ValueError("Price can't be negative.")
     if is_member:
-        return price - 0.10
+        return price * 0.9
+    
     return price
